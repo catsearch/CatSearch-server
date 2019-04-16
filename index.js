1333c3da0e852d5d4036e2ed9d08533ef47405f1
Cooper Barth <cooperfbarth@gmail.com>
2019-04-16 17:31:27 -0500

    database running

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(function(req, res, next) {
 
 const env = "" + process.env.NODE_ENV;
 const config = require('./config/db')["dev" || env];
-console.log(config);
 
 mongoose.connect(config.database, { useNewUrlParser: true });
 
