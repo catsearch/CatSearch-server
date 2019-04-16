06fdcab8230952d255de2b5d2b3890dac5cc007d
Cooper Barth <cooperfbarth@gmail.com>
2019-04-16 15:19:07 -0500

    started setup

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ A User-Friendly Platform for Finding Roommates at Northwestern University.
 Developed by Cooper Barth, Michael Ji, and Beth Mallon.
 
 Developed through Northwestern's EECS 330: Human Computer Interaction.
+
+
+To run locally:
+
+npm install
+
+npm run dev
---
+++ config/db.js
@@ -0,0 +1,14 @@
+const config = {
+    "dev":{
+        "host":"localhost",
+        "database":"mongodb+srv://cooper-barth:<password>@catsearch-dev-xheg9.mongodb.net/test?retryWrites=true"
+    }, // Replace cooper-barth and <password> with your database user‘s (MongoDB User) credentials. In default and dev too.
+};
+
+module.exports = config;
+
+/* 
+    -Database is on MongoDB Atlas
+    user: cooperbarth2021@u.northwestern.edu
+    pw: ILoveHCI!420
+*/
