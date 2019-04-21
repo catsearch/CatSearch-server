aec4bcd0a940d980eae88773008c5f2ce7f399d4
Cooper Barth <cooperfbarth@gmail.com>
2019-04-21 00:15:53 -0500

    started html

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.set('view engine', 'html');
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
