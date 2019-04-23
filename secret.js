1abb91b23a09cbef3c06545e639444ed7664b948
Cooper Barth <cooperfbarth@gmail.com>
2019-04-23 16:42:06 -0500

    shhhhhhhhh:

---
+++ secret.js
@@ -0,0 +1,7 @@
+const info = {
+    "devDatabase": "mongodb+srv://" + "cooper-barth" + ":" + "thisisadatabase" + "@catsearch-dev-xheg9.mongodb.net/test?retryWrites=true",
+    "sessionSecret": "ThisIsNotASecret"
+};
+
+module.exports = info;
+
