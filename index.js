fa3161141a2fb6b5e34bd1643a2f2dbd35d52dc2
Cooper Barth <cooperfbarth@gmail.com>
2019-04-23 18:39:43 -0500

    back to before

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const passport = require('passport');
-require('./config/passport')(passport);
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const sessionSecret = require('./secret')["sessionSecret"];
 
 app.set('view engine', 'html');
 
@@ -14,7 +9,6 @@ const env = "" + process.env.NODE_ENV;
 const config = require('./config/db')["dev" || env];
 mongoose.connect(config.database, { useNewUrlParser: true });
 
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
@@ -24,20 +18,8 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.use(session({
-	secret: sessionSecret,
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
 const userRoutes = require('./routes/user');
-const authRoutes = require('./routes/auth');
-const testRoutes = require('./routes/test');
-app.use('/users', userRoutes);
-app.use('/auth', authRoutes);
-app.use('/test', testRoutes);
+app.use('/user', userRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
