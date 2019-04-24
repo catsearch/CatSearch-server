1e007b5f47d16573aa5d74da84d00edd72c96abd
Cooper Barth <cooperfbarth@gmail.com>
2019-04-23 19:58:19 -0500

    spooky

--- secret.js
+++
@@ -1,7 +0,0 @@
-const info = {
-    "devDatabase": "mongodb+srv://" + "cooper-barth" + ":" + "thisisadatabase" + "@catsearch-dev-xheg9.mongodb.net/test?retryWrites=true",
-    "sessionSecret": "ThisIsNotASecret"
-};
-
-module.exports = info;
-
