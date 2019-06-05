537ab0a01edfd3a5f91e5bc399f4dd1fad1b07eb
Cooper Barth <cooperfbarth@gmail.com>
2019-06-05 00:04:18 -0500

    secret no more

---
+++ secret.js
@@ -0,0 +1,9 @@
+const mongoUser = "cooper-barth";
+const mongoPw = "thisisadatabase";
+
+const info = {
+    "devDatabase": "mongodb+srv://" + mongoUser + ":" + mongoPw + "@catsearch-dev-xheg9.mongodb.net/test?retryWrites=true"
+};
+
+module.exports = info;
+
