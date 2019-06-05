5c37d014b7593dba8b26b123f2c93c296149c2de
mvj23 <michaelji2021@u.northwestern.edu>
2019-06-04 21:04:46 -0500

    8081

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const authRoutes = require('./routes/auth');
 app.use('/user', userRoutes);
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT);
 
 module.exports = app;
