1fc06d5a0a62f6583f886871b40eacb50c453262
Cooper Barth <cooper.barth@getbraintree.com>
2019-08-15 16:28:59 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+"use strict";
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const logger = require("./src/logger");
 const env = "" + process.env.NODE_ENV;
+logger.info("ENV: " + env);
+
 const config = require('./config/db')["dev" || env];
-mongoose.connect(config.database, { useNewUrlParser: true });
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, PATCH")
-	next();
+const mongoose = require('mongoose');
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, err => {
+    if (err) {
+        logger.error("Could not connect to database.");
+        logger.error(`${err.name}: ${err.errorLabels}`);
+        process.exit(1);
+    } else {
+        logger.info("Connected to database.");
+    }
 });
 
-const userRoutes = require('./routes/user');
-const authRoutes = require('./routes/auth');
+const express = require('express');
+const app = express();
 
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
+// Parses request bodies
+const bodyParser = require("body-parser");
+const bpConfig = {limit: "10mb", extended: true};
+app.use(bodyParser.urlencoded(bpConfig));
+app.use(bodyParser.json(bpConfig));
+
+// Forces non-HTTPS routes to redirect
+// To use, set up SSL and convert to an http/https server
+// const https = require("./src/middleware/https");
+// app.use(https);
+
+// Parses cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Adds CORS headers to requests
+const cors = require("./src/middleware/cors");
+app.use(cors);
+
+// Logging
+const morgan = require("./src/middleware/morgan");
+app.use(morgan.loggingErr);
+app.use(morgan.loggingOut);
+
+// Sets various security-related HTTP headers
+const helmet = require("helmet");
+app.use(helmet());
+
+const routes = require("./config/routes");
+Object.entries(routes).forEach(([route, router]) => {
+    app.use(route, router);
+});
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT);
+logger.info("Application listening on PORT: " + PORT);
 
 module.exports = app;
